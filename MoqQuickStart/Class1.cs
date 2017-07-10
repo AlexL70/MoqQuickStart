e5fb92d6345c9905f57e2be404d99e366c054e8c
AlexL70 <alexander.levinson.70@gmail.com>
2017-07-10 21:02:42 +0300

    Add project files.

---
+++ MoqQuickStart/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqQuickStart
+{
+    public class Class1
+    {
+    }
+}
