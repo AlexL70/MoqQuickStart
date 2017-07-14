72d43b8b39f4262185f5c33bbe14ac0bf3ba7c4a
AlexL70 <alexander.levinson.70@gmail.com>
2017-07-14 16:18:24 +0300

    fix regex

--- MoqQuickStart/MatchingArgsMoqingTestFixture.cs
+++ MoqQuickStart/MatchingArgsMoqingTestFixture.cs
@@ -30,7 +30,9 @@ namespace MoqQuickStart
             mock.Setup(foo => foo.Subtract(It.IsInRange(int.MinValue, -1, Range.Inclusive)))
                 .Returns(false);
             //  matching regex
-            mock.Setup(foo => foo.DoSomethingStringy(It.IsRegex("[a-d]+", RegexOptions.IgnoreCase)))
+            mock.Setup(foo => foo.DoSomethingStringy(It.IsAny<string>()))
+                .Returns("bar");
+            mock.Setup(foo => foo.DoSomethingStringy(It.IsRegex("^[a-d]+$", RegexOptions.IgnoreCase)))
                 .Returns("foo");
         }
 
@@ -59,10 +61,11 @@ namespace MoqQuickStart
             Assert.IsFalse(mock.Object.Subtract(-10));
         }
 
+        [Test]
         public void Regex()
         {
             Assert.AreEqual("foo", mock.Object.DoSomethingStringy("cbdbadda"));
-            Assert.AreNotEqual("foo", mock.Object.DoSomethingStringy("cdbafbdda"));
+            Assert.AreNotEqual("foo", mock.Object.DoSomethingStringy("hdda"));
         }
     }
 }
